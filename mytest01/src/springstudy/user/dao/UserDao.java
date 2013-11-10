0dca7e2979d3c13c2d0cdc64ac4f4c6c40a3ff6b
uuin99 <uuin99@gmail.com>
2013-11-10 18:57:55 +0900

    throw exception 수정

--- mytest01/src/springstudy/user/dao/UserDao.java
+++ mytest01/src/springstudy/user/dao/UserDao.java
@@ -26,7 +26,7 @@ public class UserDao {
 		this.dataSource = dataSource;
 	}
 	
-	public void add(User user) throws ClassNotFoundException, SQLException {
+	public void add(User user) throws SQLException {
 		Connection c= dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
@@ -40,7 +40,7 @@ public class UserDao {
 		c.close();
 	}
 	
-	public User get(String id) throws ClassNotFoundException, SQLException {
+	public User get(String id) throws SQLException {
 		Connection c= dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
