9041237d15008bbce9ff399dffc740a91a2705e9
uuin99 <uuin99@gmail.com>
2013-11-13 02:28:59 +0900

    2.3.3 포괄적인 테스트

--- mytest01/src/springstudy/user/dao/UserDaoTest.java
+++ mytest01/src/springstudy/user/dao/UserDaoTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import springstudy.user.domain.User;
 
@@ -24,22 +25,58 @@ public class UserDaoTest {
 		
 		assertThat(dao.getCount(), is(0));
 		
-		User user = new User();
-		user.setId("uuin99");
-		user.setName("오정훈");
-		user.setPassword("ojhk7615");
+		User user1 = new User("uuin99", "오정훈", "ojhk7615");
+		User user2 = new User("leegw", "이길원", "springno01");
 		
-		dao.add(user);
+		dao.add(user1);
+		dao.add(user2);
 		
-		assertThat(dao.getCount(), is(1));
+		assertThat(dao.getCount(), is(2));
 
-		User user2 = dao.get(user.getId());
+		User userget1 = dao.get(user1.getId());
 		
-		assertThat(user2.getName(), is(user.getName()));
-		assertThat(user2.getPassword(), is(user.getPassword()));
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
 		
+		User userget2 = dao.get(user2.getId());
+		
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
 	}
-
+	
+	@Test
+	public void count() throws SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		User user1 = new User("uuin99", "오정훈", "ojhk7615");
+		User user2 = new User("leegw", "이길원", "springno01");
+		User user3 = new User("bumjin", "박범진", "springno02");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+	}
+	
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getIserFailure() throws SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.get("unknown_id");
+	}
+	
 	public static void main(String[] args) {
 		JUnitCore.main("springstudy.user.dao.UserDaoTest");
 	}
