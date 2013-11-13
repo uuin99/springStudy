16c992194a9324e6975c34815808f5f3f041419e
uuin99 <uuin99@gmail.com>
2013-11-14 03:54:23 +0900

    2.4.2 DI와 테스트 - 테스트 코드에 의한 DI

--- mytest01/src/springstudy/user/dao/UserDaoTest.java
+++ mytest01/src/springstudy/user/dao/UserDaoTest.java
@@ -5,20 +5,20 @@ import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
 
 import springstudy.user.domain.User;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="/applicationContext.xml")
+@DirtiesContext
 public class UserDaoTest {
 	@Autowired
 	private ApplicationContext context;
@@ -36,7 +36,9 @@ public class UserDaoTest {
 		System.out.println(this.context);
 		System.out.println(this);
 		
-//		this.dao = context.getBean("userDao", UserDao.class);
+		DataSource dataSource = new SingleConnectionDataSource(
+				"jdbc:mysql://localhost/MySql", "root", "1q2w3e4R", true);
+		dao.setDataSource(dataSource);
 		
 		this.user1 = new User("uuin99", "오정훈", "ojhk7615");
 		this.user2 = new User("leegw", "이길원", "springno01");
